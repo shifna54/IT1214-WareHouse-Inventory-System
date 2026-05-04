3f7f7b23cf8b53af18f4b6ad9df65c63272e915a
shifna54 <aboothalib1981@gmail.com>
2026-05-04 18:16:22 +0530

    added main classs

---
+++ Item.java
@@ -0,0 +1,37 @@
+public class Item {
+    private String itemId;
+    private String itemName;
+    private int quantity;
+    private double price;
+
+    public Item(String itemId, String itemName, int quantity, double price) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String toString() {
+        return "ID: " + itemId + ", Name: " + itemName + ", Quantity: " + quantity + ", Price: " + price;
+    }
+}
