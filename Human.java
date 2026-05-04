12780bcd837367a8f085c69c26e71302e2f6c0f7
shifna54 <aboothalib1981@gmail.com>
2026-05-04 18:01:01 +0530

    Add  Human class

---
+++ Human.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+ class Student{
+	 String name;
+	 int age;
+	 
+	 Student(String name,int age){
+		 this.name=name;
+		 this.age=age;
+	 } 
+	 void displayDetails(){
+		 System.out.println("Name : "+name+" Age : "+age);
+	 } 
+	 public static void main(String args[]){
+		 Scanner sc=new Scanner(System.in);
+		 System.out.println("Enter your name : ");
+		 String name1=sc.nextLine();
+		 
+		 System.out.println("Enter your age : ");
+		 int age1=sc.nextInt();
+		 sc.nextLine();
+		 System.out.println("Enter your name : ");
+		 String name2=sc.nextLine();
+		 
+		 System.out.println("Enter your age : ");
+		 int age2=sc.nextInt();
+		 sc.nextLine();
+		 
+		 Student s1=new Student(name1,age1);
+		 Student s2=new Student(name2,age2);
+		 
+		 s1.displayDetails();
+		 s2.displayDetails();
+		 
+		 sc.close();
+		}  
+		
+ }
